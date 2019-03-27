862d2a4ee2a344efb31d4f47d820f52a4b0f89c0
Brice Ruth <bruth@flexion.us>
2019-03-26 22:47:23 -0500

    Initial integration with IFTTT webhook.
    IFTTT payload keys have to be value1/2/3 exactly!

--- gmail.js
+++ gmail.js
@@ -13,7 +13,24 @@ function searchGmail() {
   Logger.log('search-string: ' + searchString);
 
   var threads = GmailApp.search("in:inbox " + searchString);
-  for (var i=0; i < threads.length; i++) {
-    Logger.log(threads[i].getFirstMessageSubject());
-  }
+  var tmp_thread = threads[0]
+  Logger.log(tmp_thread.getFirstMessageSubject());
+
+  var payload =
+      {
+        "value1" : tmp_thread.getMessages()[0].getFrom(),
+        "value2" : tmp_thread.getMessages()[0].getDate().toLocaleTimeString(),
+        "value3" : tmp_thread.getMessages()[0].getSubject(),
+      };
+  
+  var options =
+      {
+        "method"  : "POST",
+        "payload" : payload,   
+        "followRedirects" : true,
+        "muteHttpExceptions": true
+      };
+  
+  var url = PropertiesService.getScriptProperties().getProperty('ifttt-webhook-url');
+  var result = UrlFetchApp.fetch(url, options);
 }
